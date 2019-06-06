a0dbca5ddb67268af2756d8223931421b1eed465
Andrei Darashenka <andrei@darashenka.com>
2019-06-06 18:24:54 +0200

    allow specifying InfluxDB URL with username/password, database, port

--- src/influx/influx.js
+++ src/influx/influx.js
@@ -1,6 +1,9 @@
 const Influx = require('influx');
 
-module.exports = new Influx.InfluxDB({
-	host: process.env.HOST || 'localhost',
-	database: 'lighthouse'
-});
+module.exports =
+	process.env.INFLUXDB_URL ?
+		new Influx.InfluxDB( process.env.INFLUXDB_URL )
+		: new Influx.InfluxDB({
+			host: process.env.HOST || 'localhost',
+			database: 'lighthouse'
+		});
